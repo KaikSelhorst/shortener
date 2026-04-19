229b3399e0a6eeae8808393886089e391bbb5ff6
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2026-04-19 09:15:00 -0400

    feat: add dto package with project request validation

---
+++ api/internal/dto/project.go
@@ -0,0 +1,24 @@
+package dto
+
+import (
+	"errors"
+	"regexp"
+)
+
+var validName = regexp.MustCompile(`^[\p{L}0-9 _-]+$`)
+
+type CreateProjectRequest struct {
+	Name string `json:"name"`
+}
+
+func (r *CreateProjectRequest) Validate() error {
+	if r.Name == "" {
+		return errors.New("name is required")
+	}
+
+	if !validName.MatchString(r.Name) {
+		return errors.New("name can only contain letters, digits, spaces, hyphens and underscores")
+	}
+
+	return nil
+}
