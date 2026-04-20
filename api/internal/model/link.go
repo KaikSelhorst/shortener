cd137452e355b6b58e44d31d879bf4b7d82cfe79
Kaik Selhorst <oficial.kaikselhorst@gmail.com>
2026-04-20 02:00:00 -0400

    feat: update models to use int64 ids and nullable optional fields

--- api/internal/model/link.go
+++ api/internal/model/link.go
@@ -3,11 +3,11 @@ package model
 import "time"
 
 type Link struct {
-	ID          int       `json:"id"`
-	ProjectID   int       `json:"project_id"`
+	ID          int64     `json:"id"`
+	ProjectID   int64     `json:"project_id"`
 	OriginalURL string    `json:"original_url"`
-	Title       string    `json:"title"`
-	Description string    `json:"description"`
-	OgImage     string    `json:"og_image"`
+	Title       *string   `json:"title"`
+	Description *string   `json:"description"`
+	OgImage     *string   `json:"og_image"`
 	CreatedAt   time.Time `json:"created_at"`
 }
--- api/internal/model/project.go
+++ api/internal/model/project.go
@@ -3,7 +3,7 @@ package model
 import "time"
 
 type Project struct {
-	ID        int       `json:"id"`
+	ID        int64     `json:"id"`
 	Name      string    `json:"name"`
 	Slug      string    `json:"slug"`
 	CreatedAt time.Time `json:"created_at"`
